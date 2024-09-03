bff560d988f2c73d781281cf58484daedff09d53
admin <admin@admin.com>
2024-09-03 12:04:56 +0300

    Проработка планирования

---
+++ apps/workers/views/error.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseNotFound
+from django.shortcuts import render
+from django.template import RequestContext
+from django.views import View
+from django.views.generic import TemplateView
+
+
+def error_pageNotFound_404(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена.</h1>")
+
+
+def error_errorServer_500(request, exception):
+    return HttpResponseNotFound("ошибка сервера")
+
+
+def error_noAccess_403(request, exception):
+    # if not request.user.is_staff:
+    #    print('проверка на права')
+    return render(request, "error_403.html")
+
+
+def error_error_400(request, exception):
+    return HttpResponseNotFound("невозможно обработать запрос")
+
+
+class noAccessPage(LoginRequiredMixin, TemplateView):
+    """Закрытая страница"""
+    template_name = 'page_403.html'
+
+    login_url = 'login'
+
